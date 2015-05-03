02f62e8dd712b8ef501bc97241205e5db3664a9e
Oeldin <leodintner@yahoo.de>
2015-05-03 12:29:55 +0200

    Fixed CameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -34,61 +34,34 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
-        // only update lookahead pos if accelerating or changed direction
-        
-		/*float xMoveDelta = (target.position - lastTargetPosition).x;
-        bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
-
-        if (updateLookAheadTarget)
-        {
-            lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
-        }
-        else
-        {
-            lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);
-        }
-
-        Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;
-        Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);*/
-
-		//check if player moving
-
-		// {
 
 		pos = target.position;
 		pos.z = offsetZ;
 
-		if (target.position != lastTargetPosition || notMoving > 2) {
-			Vector3 change = target.position - lastTargetPosition;
-			change.Normalize();
-			lookAheadPos = new Vector3 (lookAheadFactor * aspect * (change.x), lookAheadFactor * (change.y), 0f);
-			pos += lookAheadPos;
-		
+		Vector3 change = target.position - lastTargetPosition;
+		change.Normalize();
+		lookAheadPos = new Vector3 (lookAheadFactor * aspect * (change.x), lookAheadFactor * (change.y), 0f);
+		pos += lookAheadPos;
 
-			enemies = GameObject.FindGameObjectsWithTag ("Enemy");
-			enemyPos = Vector3.zero;
-			foreach (GameObject enemy in enemies) {
-				if (Mathf.Abs (enemyPos.x) > maxEnemyFactor || Mathf.Abs (enemyPos.y) > maxEnemyFactor)
-					break;
-				Vector3 dist = enemy.transform.position - target.position;
-				enemyPos.x += dist.x * enemyFactor;
-				enemyPos.y += dist.y * enemyFactor;
-                enemyPos.z = 0f;
-			}
-			pos += enemyPos;
-
-			transform.position = Vector3.SmoothDamp (transform.position, pos, ref currentVelocity, 0.3f);
-
-			if (target.position != lastTargetPosition){
-				notMoving = 0;
-				lastTargetPosition = target.position;
-			}
-
+		enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+		enemyPos = Vector3.zero;
+		foreach (GameObject enemy in enemies) {
+			if (Mathf.Abs (enemyPos.x) > maxEnemyFactor || Mathf.Abs (enemyPos.y) > maxEnemyFactor)
+				break;
+			Vector3 dist = enemy.transform.position - target.position;
+			enemyPos.x += dist.x * enemyFactor;
+			enemyPos.y += dist.y * enemyFactor;
+               enemyPos.z = 0f;
 		}
-		else notMoving++;
+		pos += enemyPos;
+
+		transform.position = Vector3.SmoothDamp (transform.position, pos, ref currentVelocity, 0.3f);
+
+		lastTargetPosition = target.position;
+
+
 
     }
 }
