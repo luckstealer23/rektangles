34d77fde37d63372c6f47aaa3f3754852f16ddaa
Leo Dintner <leodintner@yahoo.de>
2015-05-05 14:23:37 +0200

    Press "s" to spawn enemies
    i hope this commit doesn't break all the stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     Spawner spawner;
 
+	bool spawning;
+	Image img;
+	float timeSinceSpawn;
+
+
 	// Use this for initialization
 	void Start () {
         spawner = GetComponent<Spawner>();
+		img =  GameObject.Find("SpawningImg").GetComponent<Image>();
+		timeSinceSpawn = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Time.frameCount % 100 == 0)
-            spawner.spawnEnemy();
+		if (Input.GetKeyDown (KeyCode.S))
+			spawning = !spawning;
+        
+		spawn ();
+	}
+
+	void spawn(){
+		if (spawning && timeSinceSpawn > 1) {
+			spawner.spawnEnemy ();
+			img.color = Color.red;
+			timeSinceSpawn = 0;
+		} else {
+			img.color = Color.green;
+			timeSinceSpawn += Time.deltaTime;
+		}
 	}
 }
