56ac34e618187b9734672a4ec064e02c7c6f5592
Jacob Palecek <jaxo10@hotmail.com>
2015-05-17 13:48:41 +0200

    further continued the wavegen

--- Assets/Scripts/WaveGenerator.cs
+++ Assets/Scripts/WaveGenerator.cs
@@ -11,7 +11,8 @@ public class WaveGenerator : MonoBehaviour {
     public float WavePause;
     //Determines wether the WaveGenerator should be active (the game has started)
     public bool DoSpawnWaves = true;
-
+    //sets the Spawnpoints (will be set by the MapManager)
+    public List<Transform> SpawnPoints = new List<Transform>();
 
     int livingEnemies = 0;
     public int RandomSeed;
@@ -69,9 +70,36 @@ public class WaveGenerator : MonoBehaviour {
 
         //Makes a local copy of the WaveScript so we can mess with it
         WaveScript thisWave = wave;
+        //used to make enemies spawn at the specified rate
+        float currentSpawnTime = Time.time;
         //sorts the WaveScript by the spawnpriority of the enemies
         thisWave.EnemiesToSpawn.Sort(SortBySpawnPriority);
+        //iterates through each type of enemy by their priority
+        foreach (WaveScript.EnemyInfo currentEnemy in thisWave.EnemiesToSpawn)
+        {
+            //used so the loop runs until all enemies are finished
+            bool currentEnemyFinished = false;
+            int currentEnemiesSpawned = 0;
 
+            while (!currentEnemyFinished)
+            {
+
+               
+                if (currentEnemy.EnemyAmount < currentEnemiesSpawned)
+                {
+                    //Ensures enemies are spawned at each spawn point if possible. spawn point will be randomized if there are less enemies remaining to be spawned than there are spawnpoints
+                    //e.g. you need to spawn 3 enemies, but you have 4 spawn points --> spawns will be random;
+                    if (currentEnemy.EnemyAmount >= SpawnPoints.Count)
+                    {
+                        foreach (Transform currentSpawnPoint in SpawnPoints)
+                        {
+                            //Instantiate enemy
+                        }
+                    }
+                }
+
+            }
+        }
         
 
     }
