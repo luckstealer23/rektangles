b008e3d8e7b2ae611dd8a4aa1550696db8e58389
Jacob Palecek <jaxo10@hotmail.com>
2015-06-08 12:03:45 +0200

    Continued work on generic enemy class and stuff
    as it seems I will need to make it an abstract class for maximum
    elegancy and efficiency. I dont really have a problem with that, but its
    going to take some time

---
+++ Assets/Scripts/EnemyScripts/Enemy1Behaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1Behaviour : MonoBehaviour {
+
+    //These store important gameobjects on spawn to make sure performance is saved when there is need for them to be called
+    GameObject player;
+    Camera camera;
+    GameObject waveManager;
+    
+
+    public void StartUp (GameObject playerGO, Camera cameraCam, GameObject waveManagerGO)
+    {
+        player = playerGO;
+        camera = cameraCam;
+        waveManager = waveManagerGO;
+        Debug.Log("working nicely!!0");
+    }
+
+    public void RunLogic ()
+    {
+        //The rotation "rot" is the rotation needed for the enemy to be facing the player GO
+        Quaternion rot = Quaternion.LookRotation(transform.position - new Vector3(player.transform.position.x, player.transform.position.y, -10), Vector3.forward);
+    }
+
+}
--- Assets/Scripts/EnemyScripts/GenericEnemyScript.cs
+++ Assets/Scripts/EnemyScripts/GenericEnemyScript.cs
@@ -3,34 +3,59 @@ using System.Collections;
 
 public class GenericEnemyScript : MonoBehaviour {
 
+    //Important variables for generic enemy traits
+    public float health;
     //the total game time at which the physics interaction of the enemy will end
     float physicsEndTime;
-
+    //These store important gameobjects on spawn to make sure performance is saved when there is need for them to be called
     GameObject player;
     Camera camera;
     GameObject waveManager;
+    
 
 
-	public void StartUp()
+  
+
+	public void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         camera = GameObject.FindObjectOfType<Camera>();
         waveManager = GameObject.FindGameObjectWithTag("WaveManager");
+        
     }
 
 
 
 	void Update () {
-	    if (!PhysicsActive())
+        if (health > 0)
         {
-            //call enemy logic here
+            if (!PhysicsActive())
+            {
+                
+                //call enemy logic here
+            }
+        }
+        else
+        {
+            DestroyEnemy();
         }
 	}
 
-    void OnCollisionEnter2D (Collision2D collisions)
+    void OnCollisionEnter2D (Collision2D coll)
     {
-        //if player kill player yadayadayada
+        if (coll.collider.tag == "Player")
+        {
+            //if player kill player yadayadayada
+        }
     }
+
+
+
+    public void DestroyEnemy()
+    {
+        waveManager.GetComponent<WaveGenerator>().EnemyDied();
+    }
+
 
     //call this function to suspend player movement and let them be affected by physics
     public void ActivatePhysics (float physicsLength)
@@ -50,8 +75,6 @@ public class GenericEnemyScript : MonoBehaviour {
             return false;
         }
     }
-
-
 
 
 }
